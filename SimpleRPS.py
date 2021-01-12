c375b5e7ecbf223424b00857d6035833f7a74c20
ColesonWelles <72845030+ColesonWelles@users.noreply.github.com>
2021-01-11 19:46:58 -0700

    Add files via upload

---
+++ SimpleRPS.py
@@ -0,0 +1,19 @@
+import random
+
+def play():
+    user = input('''Choose 'r' for rock, 'p' for paper, 's' for scissors''')
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return '''It's a Tie!'''
+    
+    if is_winner(user, computer):
+        return '''You've won!'''
+
+    return '''You've lost!'''
+
+def is_winner(player, opponent):
+    if (player == 'r', and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+print(play())
